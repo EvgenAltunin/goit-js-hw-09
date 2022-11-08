43775091333ddaf7754d14db9efcc63f652f21e7
EvgenAltunin <106544836+EvgenAltunin@users.noreply.github.com>
2022-11-08 17:52:39 +0000

    Deploying to gh-pages from @ EvgenAltunin/goit-js-hw-09@f27d5af966204fb51c56b8a4911d72efebdf17dd 🚀

--- 03-promises.1e074854.js
+++
@@ -1,2 +0,0 @@
-console.log("Test console msg");
-//# sourceMappingURL=03-promises.1e074854.js.map
--- 03-promises.1f416a18.js
+++
@@ -1,2 +0,0 @@
-console.log("Test console msg");
-//# sourceMappingURL=03-promises.1f416a18.js.map
